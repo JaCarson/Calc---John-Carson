2d36ea5b0809b4e10a1a7e54bda64c60b0dc1210
JaCarson <JohnAldenCarson@gmail.com>
2016-08-09 00:10:38 -0400

    First Commit

--- Calc/main.m
+++ Calc/main.m
@@ -8,10 +8,47 @@
 
 #import <Foundation/Foundation.h>
 
+int add(int x, int y)  {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+        
+        int x = 0;
+        int y = 0;
+        
+        int op;
+        
+        NSLog(@"Enter variable 1");
+        scanf("%d", &x);
+        NSLog(@"Enter variable 2");
+        scanf("%d", &y);
+        
+        NSLog(@"Add or Subtract? Enter '1' or '2' ");
+        scanf("%d", &op);
+        
+        NSLog(@"LoL Computing....%d .... %d...", x, 7);
+        
+        if (op == 1) {
+            int z = add(x, y);
+            NSLog(@"answer: %d", z);
+            
+        } else if (op == 2 ) {
+                int z = subtract(x, y);
+                NSLog(@"Answer: %d", z);
+                
+            }
+            
+        
+        
+        
+        
+     
+}
+        return 0;
 }
